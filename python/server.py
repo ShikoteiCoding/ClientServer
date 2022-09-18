7d4ae0d427af6ab7c0a8d2a0f1dc6078ff17e1c9
Come Vitis <business.vitis@gmail.com>
2022-09-18 05:13:15 +0200

    Beautify

--- python/server.py
+++ python/server.py
@@ -5,16 +5,17 @@ from utils import HOST, PORT, SOCKSIZE
 
 if __name__ == "__main__":
 
-    server = socket.create_server((HOST, PORT), family = socket.AF_INET)
+    with socket.create_server((HOST, PORT), family = socket.AF_INET) as sock:
+        
+        sock.listen(1) # Queue size of pending connection is 1
+        conn, addr = sock.accept()
 
-    server.listen(1)
-    conn, add = server.accept()
+        with conn:
+            print(f"[INFO]: {addr} is connected.")
 
-    with conn:
-        print(f"{add} is connected.")
-        while True:
-            data = conn.recv(SOCKSIZE)
-            
-            if not data : break
-
-            conn.sendall(data)
+            while True:
+                data = conn.recv(SOCKSIZE)
+                
+                if not data : break
+                
+                conn.sendall(data + b". From server.")
