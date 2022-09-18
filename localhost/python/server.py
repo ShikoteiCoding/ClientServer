cee92fb7a3509bf1484fa92da68ec69934591a99
Come Vitis <business.vitis@gmail.com>
2022-09-18 10:35:50 +0200

    Multi connection server

--- localhost/python/server.py
+++ localhost/python/server.py
@@ -5,40 +5,57 @@ import types
 from functools import partial
 from utils import HOST, PORT, SOCKSIZE
 
-def accept_wrapper(sock: socket.socket, _, *, selector: selectors.BaseSelector) -> None:
+def accept_func(sock: socket.socket, _, *, selector: selectors.BaseSelector) -> None:
     conn, addr = sock.accept()
     print(f"[INFO]: {addr} is connected.")
     sock.setblocking(False)
     data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    selector.register(conn, events, data=data)
+    event = selectors.EVENT_READ | selectors.EVENT_WRITE
+    selector.register(conn, event, data=data)
 
-def service_connection(key, mask, *, selector: selectors.BaseSelector) -> None:
-    sock = key.fileobj
+def handler_connection_func(key, mask, *, selector: selectors.BaseSelector) -> None:
+    sock: socket.socket = key.fileobj 
     data = key.data
 
-    print("hi")
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(SOCKSIZE)
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print(f"Closing connection to {data.addr}")
+            selector.unregister(sock)
+            sock.close()
+    
+
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
 
 if __name__ == "__main__":
 
-    sel = selectors.DefaultSelector()
+    selector = selectors.DefaultSelector()
 
-    accept_func = partial(accept_wrapper, selector = sel)
-    service_connection_func = partial(service_connection, selector = sel)
+    accept = partial(accept_func, selector = selector)
+    handler_connection = partial(handler_connection_func, selector = selector)
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        print(f"[INFO]: Server is running... Ready to accept connections.")
+
         sock.bind((HOST, PORT)) # Bind host and port to socket
         sock.listen(1) # Queue size of pending connection is 1
 
         try:
             while True:
-                events = sel.select(timeout=None)
+                events = selector.select(timeout=None)
                 for key, mask in events:
                     if key.data is None:
-                        accept_func(key.fileobj, None) # type: ignore
+                        print("here")
+                        accept(key.fileobj, None) # type: ignore
                     else:
-                        service_connection_func(key, mask)
+                        handler_connection(key, mask)
         except KeyboardInterrupt:
             print("Caught keyboard interrupt, exiting")
         finally:
-            sel.close()
+            selector.close()
