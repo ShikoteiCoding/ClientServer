648c1cb11a614df3407b2e92744c2260261de8df
Come Vitis <business.vitis@gmail.com>
2022-09-18 05:16:15 +0200

    Making use of context manager

--- python/server.py
+++ python/server.py
@@ -5,8 +5,9 @@ from utils import HOST, PORT, SOCKSIZE
 
 if __name__ == "__main__":
 
-    with socket.create_server((HOST, PORT), family = socket.AF_INET) as sock:
-        
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.bind((HOST, PORT))
+
         sock.listen(1) # Queue size of pending connection is 1
         conn, addr = sock.accept()
 
