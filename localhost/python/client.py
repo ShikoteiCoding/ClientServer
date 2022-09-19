9848ef5fc2e121303c8c6c589b09d76082a40e2b
Come Vitis <business.vitis@gmail.com>
2022-09-19 10:36:19 +0200

    WIP: Client with multiple connections

--- localhost/python/client.py
+++ localhost/python/client.py
@@ -1,12 +1,34 @@
 import socket
+import types
+import selectors
 
 from utils import HOST, PORT, SOCKSIZE
 
+MESSAGES = [b"First Message from Client.", b"Second Message from Client."]
+
+def generate_connections(nb: int = 10) -> None:
+    for i in range(nb):
+        print(f"[INFO]: Client {i} is ready to connect ...")
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.setblocking(False)
+            sock.connect_ex((HOST, PORT))
+            events = selectors.EVENT_READ | selectors.EVENT_WRITE
+            data = types.SimpleNamespace(
+                connid=i,
+                msg_total=sum(len(m) for m in MESSAGES),
+                recv_total=0,
+                messages=MESSAGES.copy(),
+                outb=b"",
+            )
+            selector.register(sock, events, data=data)
+
+
+
+
+
 if __name__ == "__main__":
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((HOST, PORT))
-        sock.sendall(b"I am a client.")
-        data = sock.recv(SOCKSIZE)
+    selector = selectors.DefaultSelector()
 
-    print(f"Receivef: {data}")
+    generate_connections(10)
